2e0b6279134036a67dd4c7b7edd0f9cfd3fd4730
SkyMz23 <pauljosue417@gmail.com>
2024-02-07 14:07:47 +0100

    finalisation des élément sur les sites web et enregistrement des données dans un fichiers extracted_data.json

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 // index.js
 
-// Importer les modules nécessaires
-const { program } = require('commander');
-const retrieveCommand = require('./commands/retrieve');
-const sendCommand = require('./commands/send');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const websites = require('./Data/dataWebsite.json').websites;
 
-// Configurer les commandes de la ligne de commande
-program.addCommand(retrieveCommand);
-program.addCommand(sendCommand);
+async function scrapeWebsite(url) {
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        
+        const extractedData = {
+            url: url,
+            paragraphs:[],
+            links:[],
+            h1Titles:[]
+        };
 
-// Analyser les arguments de la ligne de commande
-program.parse(process.argv);
+        // Récupération des paragraphes
+        $('p').each((index,element)=>{
+            extractedData.paragraphs.push($(element).text());
+        });
+
+        //Récupération des Grands Titres
+        $('h1').each((index,element)=>{
+            extractedData.paragraphs.push($(element).text());
+        });
+
+        //Récupération des liens
+        $('a').each((index, element)=>{
+            extractedData.paragraphs.push($(element).text())
+        })
+
+        return extractedData; // Retourne les données extraites
+    } catch (error) {
+        console.error('Error scraping website:', error);
+        return null; // Retourne un tableau vide en cas d'erreur
+    }
+}
+
+async function scrapeAllWebsites() {
+    const allExtractedData = [];
+
+    for (const url of websites) {
+        const extractedData = await scrapeWebsite(url);
+        if (extractedData){
+            allExtractedData.push(extractedData); // Ajoute les données extraites avec l'URL correspondante
+        }
+    }
+
+    return allExtractedData; // Retourne le tableau contenant toutes les données extraites
+}
+
+scrapeAllWebsites().then((allData) => {
+    const jsonData = JSON.stringify(allData, null, 2); // Convertit le tableau en JSON avec une indentation de 2 espaces
+    fs.writeFileSync('extracted_data.json', jsonData); // Écrit le JSON dans un fichier
+    console.log('Data extracted and saved to extracted_data.json');
+}).catch((error) => {
+    console.error('Error scraping all websites:', error);
+});
+
